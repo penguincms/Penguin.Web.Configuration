00e0193ca6072513defd0321a1b59af6374d78b8
anloffredo <loffredo.ap@gmail.com>
2019-10-23 00:53:54 -0700

    Framework extraction update
    Framework extraction update

---
+++ Attributes/RequiresConfigurationAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Penguin.Configuration.Abstractions.Interfaces;
+using Penguin.Web.Configuration.Exceptions;
+
+namespace Penguin.Web.Configuration.Attributes
+{
+    public sealed class RequiresConfigurationAttribute : ActionFilterAttribute, IActionFilter
+    {
+        public string Configuration { get; set; }
+
+        public bool Value { get; set; }
+
+        public RequiresConfigurationAttribute(string configuration, bool value)
+        {
+            this.Configuration = configuration;
+            this.Value = value;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext is null)
+            {
+                throw new System.ArgumentNullException(nameof(filterContext));
+            }
+
+            bool ConfigParsed = bool.TryParse(filterContext.HttpContext.RequestServices.GetService<IProvideConfigurations>().GetConfiguration(this.Configuration), out bool val);
+
+            if ((ConfigParsed && val != this.Value) || (this.Value && !ConfigParsed))
+            {
+                throw new FailedConfigurationCheckException();
+            }
+            else
+            {
+                base.OnActionExecuting(filterContext);
+            }
+        }
+    }
+}
---
+++ Exceptions/FailedConfigurationCheckException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Penguin.Web.Configuration.Exceptions
+{
+    public class FailedConfigurationCheckException : Exception
+    {
+        public FailedConfigurationCheckException()
+        {
+        }
+
+        public FailedConfigurationCheckException(string message) : base(message)
+        {
+        }
+
+        public FailedConfigurationCheckException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected FailedConfigurationCheckException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
